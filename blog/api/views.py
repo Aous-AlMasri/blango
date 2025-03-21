40302a1eafab372c6b944df98f7c90f1594c1203
Aws Masri <aousmasri77@gmail.com>
2025-03-21 14:35:10 +0000

    Finish permissions

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,12 +1,15 @@
 from rest_framework import generics
 
 from blog.api.serializers import PostSerializer
+from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blog.models import Post
+
 
 class PostList(generics.ListCreateAPIView):
   queryset = Post.objects.all()
   serializer_class = PostSerializer
 
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+  permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
   queryset = Post.objects.all()
   serializer_class = PostSerializer
