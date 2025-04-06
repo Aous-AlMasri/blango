ceeeec75ce90f7b69c54ca9f85a2e2887ef0544f
Aws Masri <aousmasri77@gmail.com>
2025-04-06 23:48:21 +0000

    Finish django-versatileimagefield

--- blango/settings.py
+++ blango/settings.py
@@ -61,7 +61,8 @@ class Dev(Configuration):
         'rest_framework',
         'rest_framework.authtoken',
         'drf_yasg',
-        'django_filters'
+        'django_filters',
+        'versatileimagefield'
     ]
 
     REST_FRAMEWORK = {
@@ -244,6 +245,10 @@ class Dev(Configuration):
     # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
     STATIC_URL = '/static/'
+
+    MEDIA_ROOT = BASE_DIR / "media"
+
+    MEDIA_URL = "/media/"
 
     # Default primary key field type
     # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
