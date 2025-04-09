0c8db73d165d408e7c05f785b69899b79be3d8b7
Aws Masri <aousmasri77@gmail.com>
2025-04-09 11:15:19 +0000

    Finish JavaScript classes

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,10 +1,45 @@
-const numbers = [0,2,3,4,5,7,8,9]
+class Greeter {
+  constructor (name) {
+    this.name = name
+  }
 
-numbers.forEach((value => {
-  console.log(value)
-}))
+  getGreeting() {
+    if (this.name === undefined) {
+      return 'hello, no name'
+    }
+    return 'hello, ' + this.name
+  }
 
+  showGreeting (greetMessage) {
+    console.log(greetMessage)
+  }
 
-const doubled = numbers.map(value => value * 2)
+  greet() {
+    this.showGreeting(this.getGreeting())
+  }
+}
 
-console.log(doubled)
+class DelayedGreeter extends Greeter {
+  delay = 2000
+
+  constructor (name, delay) {
+    super(name)
+    if (delay !== undefined) {
+      this.delay = delay
+    }
+  }
+
+  greet () {
+    setTimeout(() => {
+      this.showGreeting(this.getGreeting())
+    }, this.delay)
+  }
+}
+
+const dg1 = new DelayedGreeter('aous', 6000)
+
+dg1.greet()
+
+const dg2 = new DelayedGreeter('omar')
+
+dg2.greet()
