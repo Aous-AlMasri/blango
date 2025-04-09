33dd53a2aef1cefce77b7680f055f4dd0c34c0ac
Aws Masri <aousmasri77@gmail.com>
2025-04-09 09:37:15 +0000

    Finish JavaScript functions

--- blog/static/blog/blog.js
+++ blog/static/blog/blog.js
@@ -1,1 +1,10 @@
-alert("hey")
+const numbers = [0,2,3,4,5,7,8,9]
+
+numbers.forEach((value => {
+  console.log(value)
+}))
+
+
+const doubled = numbers.map(value => value * 2)
+
+console.log(doubled)
